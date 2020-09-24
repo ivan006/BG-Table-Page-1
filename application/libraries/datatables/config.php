01020098fda1f22aa9cbd5f3fa4ea0bac728d54b
ivan006 <ivan.copeland2015@gmail.com>
2020-09-24 10:42:05 +0200

    Datatables

--- application/libraries/datatables/config.php
+++ application/libraries/datatables/config.php
@@ -8,13 +8,17 @@ ini_set('display_errors', '1');
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Database user / pass
  */
+
+$this->load->database();
+$ci_db = $this->db;
+
 $sql_details = array(
 	"type" => "Mysql",     // Database type: "Mysql", "Postgres", "Sqlserver", "Sqlite" or "Oracle"
-	"user" => "",          // Database user name
-	"pass" => "",          // Database password
-	"host" => "", // Database host
-	"port" => "",          // Database connection port (can be left empty for default)
-	"db"   => "",          // Database name
+	"user" => $ci_db->username,          // Database user name
+	"pass" => $ci_db->password,          // Database password
+	"host" => $ci_db->hostname, // Database host
+	"port" => "3306",          // Database connection port (can be left empty for default)
+	"db"   => $ci_db->database,          // Database name
 	"dsn"  => "",          // PHP DSN extra information. Set as `charset=utf8mb4` if you are using MySQL
 	"pdoAttr" => array()   // PHP PDO attributes array. See the PHP documentation for all options
 );
