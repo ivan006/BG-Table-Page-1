3f9bc96d0c1b8997da05b7e0bbc94f9e39d5997a
ivan006 <ivan.copeland2015@gmail.com>
2020-09-30 21:09:01 +0200

    Datatables - g_relate

--- application/modules/g_relate/controllers/record_c.php
+++ application/modules/g_relate/controllers/record_c.php
@@ -23,25 +23,25 @@ class Record_c extends MY_Controller
 		$header["title"] = $overview_table_singular." ".$record_id;
 
 		$body["relationships"] = array();
-		$body["relationships"]["overview"]["table"] = $table;
-		$body["relationships"]["overview"]["table_singular"] = $overview_table_singular;
+		$body["relationships"]["overview"]["name"]["plural"] = $table;
+		$body["relationships"]["overview"]["name"]["singular"] = $overview_table_singular;
 
 		$haystack = "id";
 		$needle = $record_id;
-		$body["overview"] = array(
+		$body["overview"]["data"] = array(
 			"table_fetch" => "fetch_where/h/$haystack/n/$needle",
 			"group_name" => "overview",
 			"rows" => $this->g_tbls->table_rows($table),
 			"table" => $table,
 		);
 
-		$children_groups = $this->relationships($body["overview"]["rows"], "_children");
+		$children_groups = $this->relationships($body["overview"]["data"]["rows"], "_children");
 
-		$body["relationships"]["parent_groups"] = $this->relationships($body["overview"]["rows"], "_id");
+		$body["relationships"]["parent_groups"] = $this->relationships($body["overview"]["data"]["rows"], "_id");
 		$body["relationships"]["child_dedi_groups"] = array();
 		$body["relationships"]["child_shared_groups"] = array();
 		foreach ($children_groups as $key => $value) {
-			if ($this->g_migrate->endsWith($value["table"], "_links")) {
+			if ($this->g_migrate->endsWith($value["name"]["plural"], "_links")) {
 				$body["relationships"]["child_shared_groups"][] = $value;
 			} else {
 				$body["relationships"]["child_dedi_groups"][] = $value;
@@ -50,7 +50,9 @@ class Record_c extends MY_Controller
 
 
 
-		$body["child_shared_groups"] = array();
+		// header('Content-Type: application/json');
+		// echo json_encode($body, JSON_PRETTY_PRINT);
+		// exit;
 		foreach ($body["relationships"]["child_shared_groups"] as $key => $value) {
 
 			$haystack = $overview_table_singular."_id";
@@ -58,56 +60,54 @@ class Record_c extends MY_Controller
 			$group_name_suffix = "_shared_children";
 
 
-			$body["child_shared_groups"][$key]["linking"] = array(
-				"rows" => $this->g_tbls->table_rows($value['table']),
-				"table" => $value['table'],
+			$body["child_shared_groups"][$key]["data"]["linking"] = array(
+				"rows" => $this->g_tbls->table_rows($value["name"]["plural"]),
+				"table" => $value["name"]["plural"],
 			);
 
-			$body["child_shared_groups"][$key]["lookup"] = array(
-				"rows" => $this->g_tbls->table_rows($value['table']),
-				"table" => $value['table'],
+			$body["child_shared_groups"][$key]["data"]["lookup"] = array(
+				"rows" => $this->g_tbls->table_rows($value["name"]["plural"]),
+				"table" => $value["name"]["plural"],
 			);
 
-			$body["child_shared_groups"][$key]["join"] = array(
+			$body["child_shared_groups"][$key]["data"]["join"] = array(
 				"rows" => "",
 			);
 
-			$body["child_shared_groups"][$key]["linking"]["rows"];
-			$body["overview"]["table"];
+			// $body["child_shared_groups"][$key]["data"]["linking"]["rows"];
+			// $body["overview"]["name"]["plural"];
 
-			$body["relationships"]["overview"]["table_singular"];
-			$body["relationships"]["overview"]["table"];
-			$body["child_shared_groups"][$key]["table_fetch"] = "fetch_join_where/t/whens/k/when_id/h/$haystack/n/$needle";
-			$body["child_shared_groups"][$key]["group_name"] = $value['table_singular'].$group_name_suffix;
+			// $body["relationships"]["overview"]["table_singular"];
+			// $body["relationships"]["overview"]["name"]["plural"];
+			$body["child_shared_groups"][$key]["data"]["table_fetch"] = "fetch_join_where/t/whens/k/when_id/h/$haystack/n/$needle";
+			$body["child_shared_groups"][$key]["data"]["group_name"] = $value['table_singular'].$group_name_suffix;
 		}
 
-		$body["child_dedi_groups"] = array();
 		foreach ($body["relationships"]["child_dedi_groups"] as $key => $value) {
 
 			$haystack = $overview_table_singular."_id";
 			$needle = $record_id;
 			$group_name_suffix = "_dedicated_children";
 
-			$body["child_dedi_groups"][] = array(
+			$body["child_dedi_groups"][]["data"] = array(
 				"table_fetch" => "fetch_where/h/$haystack/n/$needle",
 				"group_name" => $value['table_singular'].$group_name_suffix,
-				"rows" => $this->g_tbls->table_rows($value['table']),
-				"table" => $value['table'],
+				"rows" => $this->g_tbls->table_rows($value["name"]["plural"]),
+				"table" => $value["name"]["plural"],
 			);
 		}
 
-		$body["parents"] = array();
 		foreach ($body["relationships"]["parent_groups"] as $key => $value) {
 
 			$haystack = "id";
 			$needle = $overview_record[$value['foreign_key']];
 			$group_name_suffix = "_parent";
 
-			$body["parents"][] = array(
+			$body["parent_groups"][]["data"] = array(
 				"table_fetch" => "fetch_where/h/$haystack/n/$needle",
 				"group_name" => $value['table_singular'].$group_name_suffix,
-				"rows" => $this->g_tbls->table_rows($value['table']),
-				"table" => $value['table'],
+				"rows" => $this->g_tbls->table_rows($value["name"]["plural"]),
+				"table" => $value["name"]["plural"],
 			);
 		}
 
@@ -124,7 +124,7 @@ class Record_c extends MY_Controller
 		foreach ($body["child_shared_groups"] as $key => $value) {
 			$this->load->view('table_block_v', $value);
 		}
-		foreach ($body["parents"] as $key => $value) {
+		foreach ($body["parent_groups"] as $key => $value) {
 			$this->load->view('table_block_v', $value);
 		}
 		$this->load->view('table_footer_v');
@@ -140,9 +140,8 @@ class Record_c extends MY_Controller
 				$suffix_strlen = strlen($suffix);
 				$value_singular = substr($value, 0, -$suffix_strlen);
 				$relation_table = $this->g_migrate->grammar_plural($value_singular);
-				$result[$key]['table'] = $relation_table;
-				$result[$key]['table_singular'] = $value_singular;
-				$result[$key]['foreign_key'] = $value;
+				$result[$key]["name"]["plural"] = $relation_table;
+				$result[$key]["name"]["singular"] = $value_singular;
 			}
 		}
 		return $result;
